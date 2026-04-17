cd9cf9efccefe9567521361fc5635d06e0239fd3
流漓客 <2743266947@qq.com>
2026-04-17 16:42:09 +0800

    feat: 新增指标 greenplum_node_segment_disk_sum_free_mb_size 统计每个节点的总剩余空间（MB） greenplum_node_segment_disk_sum_device_free_mb_size 按设备维度统计每个节点每个设备的剩余空间（MB） fix: 修复指标 greenplum_node_segment_disk_free_mb_size label值存在空格

--- collector/segment.go
+++ collector/segment.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"github.com/prometheus/client_golang/prometheus"
 	logger "github.com/prometheus/common/log"
+	"strings"
 	"time"
 )
 
@@ -14,10 +15,14 @@ import (
  */
 
 const (
-	segmentConfigSql_V6       = `select dbid,content,role,preferred_role,mode,status,port,hostname,address,datadir from gp_segment_configuration;`
-	segmentConfigSql_V5       = `select dbid,content,role,preferred_role,mode,status,port,hostname,address,null as datadir from gp_segment_configuration;`
+	segmentConfigSql_V6 = `select dbid,content,role,preferred_role,mode,status,port,hostname,address,datadir from gp_segment_configuration;`
+	segmentConfigSql_V5 = `select dbid,content,role,preferred_role,mode,status,port,hostname,address,null as datadir from gp_segment_configuration;`
 
 	segmentDiskFreeSizeSql = `SELECT dfhostname as segment_hostname,sum(dfspace)/count(dfspace)/(1024*1024) as segment_disk_free_gb from gp_toolkit.gp_disk_free GROUP BY dfhostname;`
+
+	segmentDiskSumFreeSizeSql = `SELECT dfhostname as segment_hostname,sum(dfspace)/(1024*1024) as segment_disk_sum_free_mb from gp_toolkit.gp_disk_free GROUP BY dfhostname;`
+
+	segmentDiskDeviceFreeSizeSql = `SELECT dfhostname as segment_hostname,dfdevice as device,dfspace/(1024*1024) as segment_disk_device_free_mb from gp_toolkit.gp_disk_free;`
 )
 
 var (
@@ -42,8 +47,22 @@ var (
 	segmentDiskFreeSizeDesc = prometheus.NewDesc(
 		prometheus.BuildFQName(namespace, subSystemNode, "segment_disk_free_mb_size"), //指标的名称
 		"Total MB size of each segment node free size of disk in the file system",     //帮助信息，显示在指标的上面作为注释
-		[]string{"hostname"},                                                          //定义的label名称数组
-		nil,                                                                           //定义的Labels
+		[]string{"hostname"}, //定义的label名称数组
+		nil,                  //定义的Labels
+	)
+
+	segmentDiskSumFreeSizeDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, subSystemNode, "segment_disk_sum_free_mb_size"),
+		"Total sum MB size of each segment node free disk space across all devices",
+		[]string{"hostname"},
+		nil,
+	)
+
+	segmentDiskDeviceFreeSizeDesc = prometheus.NewDesc(
+		prometheus.BuildFQName(namespace, subSystemNode, "segment_disk_sum_device_free_mb_size"),
+		"Free MB size of each segment node per device",
+		[]string{"hostname", "device"},
+		nil,
 	)
 )
 
@@ -60,8 +79,10 @@ func (segmentScraper) Name() string {
 func (segmentScraper) Scrape(db *sql.DB, ch chan<- prometheus.Metric, ver int) error {
 	errU := scrapeSegmentConfig(db, ch, ver)
 	errC := scrapeSegmentDiskFree(db, ch)
+	errS := scrapeSegmentDiskSumFree(db, ch)
+	errD := scrapeSegmentDiskDeviceFree(db, ch)
 
-	return combineErr(errC, errU)
+	return combineErr(errC, errU, errS, errD)
 }
 
 func scrapeSegmentConfig(db *sql.DB, ch chan<- prometheus.Metric, ver int) error {
@@ -70,9 +91,9 @@ func scrapeSegmentConfig(db *sql.DB, ch chan<- prometheus.Metric, ver int) error
 
 	defer cancel()
 
-	querySql:=segmentConfigSql_V6
-	if ver < 6{
-		querySql=segmentConfigSql_V5;
+	querySql := segmentConfigSql_V6
+	if ver < 6 {
+		querySql = segmentConfigSql_V5
 	}
 
 	logger.Infof("Query Database: %s", querySql)
@@ -133,7 +154,79 @@ func scrapeSegmentDiskFree(db *sql.DB, ch chan<- prometheus.Metric) error {
 			continue
 		}
 
+		hostName = strings.TrimSpace(hostName)
 		ch <- prometheus.MustNewConstMetric(segmentDiskFreeSizeDesc, prometheus.GaugeValue, mbSize, hostName)
+	}
+
+	return combineErr(errs...)
+}
+
+func scrapeSegmentDiskSumFree(db *sql.DB, ch chan<- prometheus.Metric) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*2)
+
+	defer cancel()
+
+	logger.Infof("Query Database: %s", segmentDiskSumFreeSizeSql)
+	rows, err := db.QueryContext(ctx, segmentDiskSumFreeSizeSql)
+
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	errs := make([]error, 0)
+
+	for rows.Next() {
+		var hostName string
+		var mbSize float64
+
+		err := rows.Scan(&hostName, &mbSize)
+
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		hostName = strings.TrimSpace(hostName)
+		ch <- prometheus.MustNewConstMetric(segmentDiskSumFreeSizeDesc, prometheus.GaugeValue, mbSize, hostName)
+	}
+
+	return combineErr(errs...)
+}
+
+func scrapeSegmentDiskDeviceFree(db *sql.DB, ch chan<- prometheus.Metric) error {
+	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(ctx, time.Second*2)
+
+	defer cancel()
+
+	logger.Infof("Query Database: %s", segmentDiskDeviceFreeSizeSql)
+	rows, err := db.QueryContext(ctx, segmentDiskDeviceFreeSizeSql)
+
+	if err != nil {
+		return err
+	}
+
+	defer rows.Close()
+
+	errs := make([]error, 0)
+
+	for rows.Next() {
+		var hostName, device string
+		var mbSize float64
+
+		err := rows.Scan(&hostName, &device, &mbSize)
+
+		if err != nil {
+			errs = append(errs, err)
+			continue
+		}
+
+		hostName = strings.TrimSpace(hostName)
+		device = strings.TrimSpace(device)
+		ch <- prometheus.MustNewConstMetric(segmentDiskDeviceFreeSizeDesc, prometheus.GaugeValue, mbSize, hostName, device)
 	}
 
 	return combineErr(errs...)
